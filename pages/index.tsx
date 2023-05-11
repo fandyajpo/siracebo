c2dd66038bd91e28a768f9b8313e87a36332f2d2
fandyajpo <fandyglitch3@gmail.com>
2023-05-12 07:16:21 +0800

    sac

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ const Home = () => {
       <Head>
         <link href="../public/css/landing.css" rel="stylesheet"></link>
       </Head>
-      <div className="w-full fized z-50 h-screen bg-white">pay me</div>
       <header>
         <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-green-800">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
